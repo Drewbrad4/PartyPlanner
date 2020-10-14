ea2e5f22b2976457e2209ce49f00e21f0015ec7e
drewbrad4 <drewbrad4@gmail.com>
2020-10-13 18:38:30 -0700

    Updates

---
+++ precache-manifest.8e6b0e91ce0d358b3de1179dca37bbbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306e644d569cf8cd2262ca18f613728f",
+    "url": "/PartyPlanner/index.html"
+  },
+  {
+    "revision": "f5ac0c9730348a5861ea",
+    "url": "/PartyPlanner/static/css/main.f9e31419.chunk.css"
+  },
+  {
+    "revision": "90f37f3eadc05e4c3348",
+    "url": "/PartyPlanner/static/js/2.4236ed0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PartyPlanner/static/js/2.4236ed0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ac0c9730348a5861ea",
+    "url": "/PartyPlanner/static/js/main.cd1bcfdd.chunk.js"
+  },
+  {
+    "revision": "262b0ad7a9b000b7732b",
+    "url": "/PartyPlanner/static/js/runtime-main.83d305a7.js"
+  }
+]);
